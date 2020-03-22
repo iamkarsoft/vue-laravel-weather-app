d7b1a0c536f6822126481f24316f25b6cb729cd0
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-03-22 15:55:08 +0000

    core: initial settings

---
+++ resources/js/components/WeatherApp.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+      Hello This is a vue component
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
